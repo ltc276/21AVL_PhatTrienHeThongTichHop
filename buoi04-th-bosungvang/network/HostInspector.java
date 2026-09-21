7ae14a45e7302d59544ebe9852468e9aac32d969
letancuong2706 <133195559+ltc276@users.noreply.github.com>
2026-09-21 19:33:02 +0700

    buoi04-th-bosungvang

---
+++ buoi04-th-bosungvang/network/HostInspector.java
@@ -0,0 +1,34 @@
+package network;
+ import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class HostInspector {
+   public static void main(String[] args){
+    if(args.length!=1){
+        System.out.println("Usage: java network.HostInspector <hostname>");
+        return ;
+    }
+    try{
+        InetAddress[] addresses = InetAddress.getAllByName(args [0]);
+        System.out.print("Host: " +args[0]);
+
+        for (InetAddress address : addresses){
+            System.out.println("-IP:" + address.getHostAddress());
+
+            System.out.println(
+                "Canonical:" + address.getCanonicalHostName()
+            );
+            System.out.println(
+                "Loopback: "+ address.isLoopbackAddress()
+            );
+            System.out.println(
+                "Site local: "+address.isSiteLocalAddress()
+            );
+        }
+    }
+    catch (UnknownHostException e){
+        System.err.print("Khong phan gia duoc host:" + args[0]);
+    }
+   }
+
+}
